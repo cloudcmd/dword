cf1217adcb78107a3b215fa355455a458fb36c60
coderaiser <mnemonic.enemy@gmail.com>
2015-02-19 09:26:43 -0500

    feature(edo) add

---
+++ bin/edward.js
@@ -0,0 +1,121 @@
+#!/usr/bin/env node
+
+(function() {
+    'use strict';
+    
+    var fs              = require('fs'),
+        path            = require('path'),
+        rendy           = require('rendy'),
+        args            = process.argv.slice(2),
+        arg             = args[0];
+        
+    if (!arg)
+        usage();
+    else if (/^(-v|--v)$/.test(arg))
+        version();
+    else if (/^(-h|--help)$/.test(arg))
+        help();
+    else
+        checkFile(arg, function(error) {
+           if (!error)
+                main(arg);
+            else
+                console.error(error.message);
+       });
+    
+    function main(name) {
+        var socket,
+            edSocket,
+            cwd         = process.cwd() + '/',
+            filename    = path.normalize(cwd + name),
+            DIR         = __dirname + '/../assets/',
+            edward      = require('../'),
+            http        = require('http'),
+            express     = require('express'),
+            io          = require('socket.io'),
+            restafary   = require('restafary'),
+            
+            app         = express(),
+            server      = http.createServer(app),
+            
+            port        =   process.env.PORT            ||  /* c9           */
+                            process.env.app_port        ||  /* nodester     */
+                            process.env.VCAP_APP_PORT   ||  /* cloudfoundry */
+                            1337,
+            ip          =   process.env.IP              ||  /* c9           */
+                            '0.0.0.0';
+        
+        app .use(restafary({
+                prefix: '/api/v1/fs'
+            }))
+            .use(express.static(DIR))
+            .use(edward({
+                minify: false,
+                online: false
+            }));
+        
+        server.listen(port, ip);
+        
+        socket      = io.listen(server),
+        edSocket    = edward.listen(socket);
+        
+        edSocket.on('connection', function() {
+            fs.readFile(name, 'utf8', function(error, data) {
+                if (error)
+                    console.error(error.message);
+                else
+                    edSocket.emit('file', filename, data);
+                });
+        });
+        
+        console.log('url: http://' + ip + ':' + port);
+    }
+    
+    function checkFile(name, callback) {
+        var ERROR_ENOENT    = 'Error: no such file or directory: \'{{ name }}\'',
+            ERROR_ISDIR     = 'Error: \'{{ name }}\' is directory';
+        
+        fs.stat(name, function(error, stat) {
+            var msg;
+            
+            if (error && error.code === 'ENOENT')
+                msg = ERROR_ENOENT;
+            else if (stat.isDirectory())
+                msg = ERROR_ISDIR;
+                
+            if (msg)
+                error = {
+                    message: rendy(msg, {
+                        name: arg
+                    })
+                };
+            
+            callback(error);
+        });
+    }
+    
+    function version() {
+        console.log('v' + info().version);
+    }
+    
+    function info() {
+        return require('../package');
+    }
+    
+    function usage() {
+        var msg = 'Usage: ' + info().name + ' [filename]';
+        console.log(msg);
+    }
+    
+    function help() {
+        var bin         = require('../json/bin');
+            
+        usage();
+        console.log('Options:');
+        
+        Object.keys(bin).forEach(function(name) {
+            var line = '  ' + name + ' ' + bin[name];
+            console.log(line);
+        });
+    }
+})();
---
+++ modules/execon/lib/exec.js
@@ -0,0 +1,217 @@
+(function(global) {
+    'use strict';
+    
+    if (typeof module === 'object' && module.exports)
+        module.exports = new ExecProto();
+    else
+        global.exec = new ExecProto();
+        
+    function ExecProto() {
+        var slice = Array.prototype.slice,
+            /**
+             * function do save exec of function
+             * @param callback
+             * @param arg1
+             * ...
+             * @param argN
+             */
+            exec        = function(callback) {
+                var ret,
+                    isFunc  = typeof callback === 'function',
+                    args    = slice.call(arguments, 1);
+               
+                if (isFunc)
+                    ret     = callback.apply(null, args);
+                
+                return ret;
+            };
+        
+        /*
+         * return function that calls callback with arguments
+         */
+        
+        exec.with           =  function(callback) {
+            var result,
+                bind        = Function.bind;
+            
+            arguments[0]    = null;
+            result          = bind.apply(callback, arguments);
+            
+            return result;
+        };
+         
+         /**
+         * return save exec function
+         * @param callback
+         */
+        exec.ret        = function() {
+            var result,
+                args        = slice.call(arguments);
+            
+            args.unshift(exec);
+            result          = exec.with.apply(null, args);
+            
+            return result;
+        };
+        
+        /**
+         * function do conditional save exec of function
+         * @param condition
+         * @param callback
+         * @param func
+         */
+        exec.if         = function(condition, callback, func) {
+            var ret;
+            
+            if (condition)
+                exec(callback);
+            else
+                exec(func, callback);
+            
+            return ret;
+        };
+        
+        /**
+         * exec function if it exist in object
+         * 
+         * @param obj
+         * @param name
+         * @param arg
+         */
+        exec.ifExist                = function(obj, name, arg) {
+            var ret,
+                func    = obj && obj[name];
+            
+            if (func)
+                func    = func.apply(obj, arg);
+            
+            return ret;
+        };
+        
+        exec.parallel   = function(funcs, callback) {
+            var ERROR       = 'could not be empty!',
+                keys        = [],
+                callbackWas = false,
+                arr         = [],
+                obj         = {},
+                count       = 0,
+                countFuncs  = 0,
+                type        = getType(funcs);
+            
+            if (!funcs)
+                throw(Error('funcs' + ERROR));
+            
+            if (!callback)
+                throw(Error('callback' + ERROR));
+            
+            switch(type) {
+            case 'array':
+                countFuncs  = funcs.length;
+                
+                funcs.forEach(function(func, num) {
+                    exec(func, function() {
+                        checkFunc(num, arguments, arr);
+                    });
+                });
+                break;
+            
+            case 'object':
+                keys        = Object.keys(funcs);
+                countFuncs  = keys.length;
+                
+                keys.forEach(function(name) {
+                    var func    = funcs[name];
+                    
+                    exec(func, function() {
+                        checkFunc(name, arguments, obj);
+                    });
+                });
+                break;
+            }
+            
+            function checkFunc(num, data, all) {
+                var args    = slice.call(data, 1),
+                    isLast  = false,
+                    error   = data[0],
+                    length  = args.length;
+                
+                ++count;
+                
+                isLast = count === countFuncs;
+                
+                if (!error)
+                    if (length >= 2)
+                        all[num] = args;
+                    else
+                        all[num] = args[0];
+                
+                if (!callbackWas && (error || isLast)) {
+                    callbackWas = true;
+                    
+                    if (type === 'array')
+                        callback.apply(null, [error].concat(all));
+                    else
+                        callback(error, all);
+                }
+            }
+        };
+        
+        /**
+         * load functions thrue callbacks one-by-one
+         * @param funcs {Array} - array of functions
+         */
+        exec.series             = function(funcs, callback) {
+            var fn,
+                done,
+                i           = funcs.length,
+                isArray     = getType(funcs) === 'array',
+                check       = function(error) {
+                    --i;
+                    
+                    if (!i && !done || error) {
+                        done = true;
+                        exec(callback, error);
+                    }
+                    
+                    return done;
+                };
+            
+            if (isArray) {
+                fn = funcs.shift();
+                
+                exec(fn, function(error) {
+                    if (!check(error))
+                        exec.series(funcs, callback);
+                });
+            }
+        };
+        
+       /**
+         * function execute param function in
+         * try...catch block
+         * 
+         * @param callback
+         */
+        exec.try                = function(callback) {
+            var ret;
+            try {
+                ret = callback();
+            } catch(error) {
+                ret = error;
+            }
+            
+            return ret;
+        };
+        
+        function getType(variable) {
+            var regExp      = new RegExp('\\s([a-zA-Z]+)'),
+                str         = {}.toString.call(variable),
+                typeBig     = str.match(regExp)[1],
+                result      = typeBig.toLowerCase();
+            
+            return result;
+        } 
+        
+        return exec;
+    }
+})(this);
