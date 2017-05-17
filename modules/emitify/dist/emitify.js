433ece9c1fcbe2615c38b78f8965b2a387e39093
coderaiser <coderaiser@cloudcmd.io>
2017-05-17 14:19:00 +0300

    feature(bower) emitify v3.1.0

--- modules/emitify/dist/emitify.js
+++ modules/emitify/dist/emitify.js
@@ -1,2 +1,171 @@
-!function(t,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";function t(){if(!(this instanceof t))return new t;this._all={}}function o(t){if("string"!=typeof t)throw Error("event should be string!")}function n(t){if("function"!=typeof t)throw Error("callback should be function!")}function e(t,e){o(t),n(e)}module.exports=t,t.prototype.on=function(t,o){var n=this._all[t];return e(t,o),n?n.push(o):this._all[t]=[o],this},t.prototype.addListener=t.prototype.on,t.prototype.once=function(t,o){var n=this;return e(t,o),n.on(t,function e(){o.apply(null,arguments),n.off(t,e)}),this},t.prototype.off=function(t,o){var n=this._all[t]||[],i=n.indexOf(o);for(e(t,o);~i;)n.splice(i,1),i=n.indexOf(o);return this},t.prototype.removeListener=t.prototype.off,t.prototype.emit=function(t){var n=[].slice.call(arguments,1),e=this._all[t];if(o(t),!e&&"error"===t)throw n[0];return e?(e.forEach(function(t){t.apply(null,n)}),this):this},t.prototype.removeAllListeners=function(t){return o(t),this._all[t]=[],this}});
+var Emitify =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/* unknown exports provided */
+/* all exports used */
+/*!************************!*\
+  !*** ./lib/emitify.js ***!
+  \************************/
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = Emitify;
+
+function Emitify() {
+    if (!(this instanceof Emitify)) return new Emitify();
+
+    this._all = {};
+}
+
+Emitify.prototype.on = function (event, callback) {
+    var funcs = this._all[event];
+
+    check(event, callback);
+
+    if (funcs) funcs.push(callback);else this._all[event] = [callback];
+
+    return this;
+};
+
+Emitify.prototype.addListener = Emitify.prototype.on;
+
+Emitify.prototype.once = function (event, callback) {
+    var self = this;
+
+    check(event, callback);
+
+    self.on(event, function fn() {
+        callback.apply(null, arguments);
+        self.off(event, fn);
+    });
+
+    return this;
+};
+
+Emitify.prototype.off = function (event, callback) {
+    var events = this._all[event] || [];
+    var index = events.indexOf(callback);
+
+    check(event, callback);
+
+    while (~index) {
+        events.splice(index, 1);
+        index = events.indexOf(callback);
+    }
+
+    return this;
+};
+
+Emitify.prototype.removeListener = Emitify.prototype.off;
+
+Emitify.prototype.emit = function (event) {
+    var args = [].slice.call(arguments, 1);
+    var funcs = this._all[event];
+
+    checkEvent(event);
+
+    if (!funcs && event === 'error') throw args[0];
+
+    if (!funcs) return this;
+
+    funcs.forEach(function (fn) {
+        fn.apply(null, args);
+    });
+
+    return this;
+};
+
+Emitify.prototype.removeAllListeners = function (event) {
+    checkEvent(event);
+
+    this._all[event] = [];
+
+    return this;
+};
+
+function checkEvent(event) {
+    if (typeof event !== 'string') throw Error('event should be string!');
+}
+
+function checkFn(callback) {
+    if (typeof callback !== 'function') throw Error('callback should be function!');
+}
+
+function check(event, callback) {
+    checkEvent(event);
+    checkFn(callback);
+}
+
+/***/ })
+/******/ ]);
 //# sourceMappingURL=emitify.js.map
